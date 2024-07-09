3ad298b02593b3c44a0434002e5cb70cc5eb40a9
colegiusto <83982339+colegiusto@users.noreply.github.com>
2024-07-09 02:38:48 -0700

    c

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,1 +1,17 @@
-Hi
+<style>
+    .container {
+        width: 100%;
+        margin: 0;
+        display: flex;
+        background-color: var(--titlebar-color);
+        font-size:min(calc(5px + 1.4vw), 22px);
+        justify-content: flex-end;
+        cursor: pointer;
+        user-select: none;
+    }
+
+</style>
+
+<nav class="container">
+    
+</nav>
