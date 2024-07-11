a8d898d6fb8791078015515caf412ebb40f1464d
colegiusto <83982339+colegiusto@users.noreply.github.com>
2024-07-10 21:08:47 -0700

    c

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,5 @@
-About
+<h2>About</h2>
+<div>
+    I'm an Undergraduate Mechanical Engineering student at UC Santa Barbara. I'm interested in
+    controls, robotics, and artificial intelligence.
+</div>
