2bf3c05d207556c52ca95e4c61800e286aa81142
colegiusto <83982339+colegiusto@users.noreply.github.com>
2024-07-11 19:47:56 -0700

    c

---
+++ src/routes/404.svelte
@@ -0,0 +1,2 @@
+<h2>404 page not found</h2>
+<a href="/">click here to return to home</a>
--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: undefined
+			fallback: "404.html"
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
